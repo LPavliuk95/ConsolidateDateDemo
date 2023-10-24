245b4cb1db99bb42188d0f1fb38a5f8533eb32b2
LPavliuk95 <147345382+LPavliuk95@users.noreply.github.com>
2023-10-24 09:10:19 -0400

    Create ConsolidateController.java

---
+++ ConsolidateController.java
@@ -0,0 +1,15 @@
+import { LightningElement, track, wire, api } from 'lwc';
+import getOrdersList from '@salesforce/apex/ConsolidateController.getWrapperClassList';
+
+export default class OrderConsolidateData extends LightningElement {
+    @api recordId;
+    @track data = [];
+    @wire(getList, {orderId: '$recordId'})
+    
+    wiredData(result) {
+
+        if (result.data) {
+            this.data = result.data;
+        }
+    }
+}
